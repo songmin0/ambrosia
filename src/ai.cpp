40e206c8ad8fcc4fd21cb755537682fe71fa84af
songmin0 <christine.song.101@gmail.com>
2021-02-05 16:54:49 -0600

    Added mob component to Egg mob

--- src/ai.cpp
+++ src/ai.cpp
@@ -2,6 +2,7 @@
 #include "ai.hpp"
 #include "tiny_ecs.hpp"
 #include "physics.hpp"
+#include "TurnSystem.hpp"
 
 void AISystem::step(float elapsed_ms, vec2 window_size_in_game_units)
 {
@@ -12,7 +13,14 @@ void AISystem::step(float elapsed_ms, vec2 window_size_in_game_units)
 	// new data structures to implement a more sophisticated Fish AI. 
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-	auto& motion = ECS::registry<PlayerComponent>.components;
+	/*auto& players = ECS::registry<PlayerComponent>;
+	for (unsigned int i = 0; i < players.components.size(); i++) {
+		ECS::Entity entity = players.entities[i];
+		auto& turnComponent = ECS::registry<TurnSystem::TurnComponent>.get(entity);
+		if (!turnComponent.hasGone) {
+
+		}
+	}*/
 	(void)elapsed_ms; // placeholder to silence unused warning until implemented
 	(void)window_size_in_game_units; // placeholder to silence unused warning until implemented
 }
