2523220ec4543fab7e041610bdc3880aba355987
songmin0 <christine.song.101@gmail.com>
2021-02-09 17:11:17 -0600

    Removed ai.hpp from world.cpp because mob movement logic wwas removed

--- src/world.cpp
+++ src/world.cpp
@@ -9,7 +9,6 @@
 #include "animation_components.hpp"
 #include "animation_system.hpp"
 #include "egg.hpp"
-#include "ai.hpp"
 #include "TurnSystem.hpp"
 #include "raoul.hpp"
 #include "EventSystem.hpp"
