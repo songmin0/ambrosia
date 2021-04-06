97763b20d5873ae8ad6ec53c550ed3b1c785c89f
Emma <e.liu@alumni.ubc.ca>
2021-04-06 11:12:08 -0700

    fix tutorial select not starting in tutorial

--- src/ui/menus.cpp
+++ src/ui/menus.cpp
@@ -228,6 +228,7 @@ void Screens::createRecipeSelectScreen(int frameBufferWidth, int frameBufferHeig
 			TransitionEvent event;
 			event.callback = []() {
 				GameStateSystem::instance().isInMainScreen = false;
+				GameStateSystem::instance().loadRecipe("tutorial");
 				GameStateSystem::instance().beginStory();
 			};
 			EventSystem<TransitionEvent>::instance().sendEvent(event);
